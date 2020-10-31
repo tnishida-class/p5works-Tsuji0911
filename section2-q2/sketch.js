81be9668fcacc8911562edf01a54006fdb932853
Tsuji0911 <bmx.tsuji@gmail.com>
2020-10-31 20:03:46 +0900

    section2-q2 finish

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,7 +5,34 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+     fill(196);
+     if(i % 2 == 0 && j % 2 == 1　|| i % 2 == 1 && j % 2 == 0){
+       fill(128);
+  　　 }
+      else{
+       fill(255) ;
+      }
+     rect(size * i, size * j, size, size);
     }
   }
+  for(let k = 0; k < 8; k++){
+    for(let l = 0; l < 8;l++){
+     if(k % 2 == 1 && l % 2 == 0 || k % 2 == 0 && l % 2 == 1){
+       fill(288,0,0)
+       ellipse(k * size + size/2, l * size + size/2, 20)
+     }
+     else if (k % 2 == 1 && l == 5 || l == 6 || l == 7){
+       fill(0)
+       ellipse((k-1) * size + size/2, l * size + size/2, 20)
+     }
+     else if (k % 2 == 1 && l == 3 || l == 4){
+       fill(128)
+       ellipse((k-1) * size + size/2, l * size + size/2, 20)
+     }
+     }
+   }
+   fill(128)
+   ellipse(187.5,112.5,20)
+   fill(0)
+   ellipse(187.5,162.5,20)
 }
